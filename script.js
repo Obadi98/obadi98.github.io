4b9c698aacc28fb10da8faaf207e175c29da29ec
Obadi98 <obadabaqleh@hotmail.com>
2016-12-03 15:21:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function myFunction() {
 	var body = document.body;
 	if(body.scrollTop > 50) {
-		document.getElementById("left").style.marginRight = "670px";
-        document.getElementById("left").style.paddingTop = "15px";
-		document.getElementById("left").style.transition = "all 0.5s";
+		document.getElementById("logo").style.marginRight = "570px";
+		document.getElementById("logo").style.marginTop = "-30px";
+		document.getElementById("logo").style.backgroundColor = "#283a59";
+		document.getElementById("logo").style.transition = "all 0.5s";
 		document.getElementById("navbar").style.transition = "all 0.5s";
 		document.getElementById("navbar").style.border = "0.5px inset white";
-		document.getElementById("js").style.transition = "all 0.5s";
 		document.getElementById("java1").style.transition = "all 0.5s";
 		document.getElementById("java2").style.transition = "all 0.5s";
 		document.getElementById("java3").style.transition = "all 0.5s";
@@ -19,7 +19,6 @@ function myFunction() {
 		document.getElementById("js2").style.transition = "all 0.5s";
 		document.getElementById("js3").style.transition = "all 0.5s";
 		document.getElementById("js4").style.transition = "all 0.5s";
-		document.getElementById("js").style.fontSize = "20px";
 		document.getElementById("navbar").style.height = "60px";
 		document.getElementById("navbar").style.backgroundColor = "#283a59";
 		document.getElementById("js1").style.color = "orange";
@@ -46,15 +45,13 @@ function myFunction() {
 		document.getElementById("js3").style.color = "white";
 	}
 	if(body.scrollTop < 50) {
-		document.getElementById("left").style.marginRight = "650px";
-		document.getElementById("left").style.paddingTop = "25px";
+	    document.getElementById("logo").style.marginRight = "550px";
+		document.getElementById("logo").style.marginTop = "-20px";
 		document.getElementById("java1").style.paddingTop = "34px";
 		document.getElementById("java2").style.paddingTop = "34px";
 		document.getElementById("java3").style.paddingTop = "34px";
 		document.getElementById("java4").style.paddingTop = "34px";
 		document.getElementById("navbar").style.border = "1px inset #636363";
-		document.getElementById("js").style.fontSize = "24px";
-		document.getElementById("js").style.marginTop = "0";
 		document.getElementById("navbar").style.height = "80px";
 		document.getElementById("navbar").style.backgroundColor = "transparent";
 		document.getElementById("js1").style.color = "white";
