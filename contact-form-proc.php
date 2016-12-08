2095109912eadd5d13412bbb9472b49d0be3b5fd
Obadi98 <obadabaqleh@hotmail.com>
2016-12-08 17:02:58 +0200

    Update contact-form-proc.php

--- contact-form-proc.php
+++ contact-form-proc.php
@@ -1,20 +1,24 @@
 <?php
-
-if(empty($_POST["submit"]))
+ 
+if(empty($_POST['submit']))
 {
-	echo "Form is not submitted!";
-	exit;
+    echo "Form is not submitted!";
+    exit;
 }
 if(empty($_POST["name"]) ||
     empty($_POST["email"]))
-	{
-		echo "Please fill the form"
-		exit;
-	}
+    {
+        echo "Please fill the form";
+        exit;
+    }
+     
 $name = $_POST["name"];
 $email = $_POST["email"];
-
-mail( "obadabaqleh@hotmail.com" , "New form submission" , "New form submission: $name, Email:$email");
-
-header ("Location: thank-you.html");
+$message = $_POST["message"];
+ 
+mail( 'obadabaqleh@hotmail.com' , 'New form submission' , 
+"New form submission: Name: $name, Email:$email: $message"  );
+ 
+header('Location: thank-you.html');
+ 
 ?>
